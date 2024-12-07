87c4a95a3760ef1c5dd36dcc0dac9706dc5c9f22
nawadeveloper <nawarajusa@gmail.com>
2024-12-07 00:41:24 -0600

    useState and useEffect added to TextInput

--- client/src/components/TextInput.tsx
+++ client/src/components/TextInput.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
+
 const TextInput = () => {
+  const [typed, setTyped] = useState("");
+
+  useEffect(() => {
+    console.log(typed);
+  }, [typed]);
   return (
     <input
+      value={typed}
+      onChange={(e) => setTyped(e.target.value)}
+      autoFocus
+      onPaste={(e) => e.preventDefault()}
       className="w-full border border-gray-200 rounded-sm p-[2px] text-lg focus:outline-none focus:border-black"
       type="text"
     />
