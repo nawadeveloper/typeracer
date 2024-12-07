5d9ef1e512efbd58a730bbba1fa61bece7d3c861
nawadeveloper <nawarajusa@gmail.com>
2024-12-06 20:43:54 -0600

    style typingArea container

--- client/src/components/TypingArea.tsx
+++ client/src/components/TypingArea.tsx
@@ -1,5 +1,15 @@
 const TypingArea = () => {
-  return <div>typing area.</div>;
+  return (
+    <div className="bg-lighterGray space-y-4 px-2 py-5 border border-gray-300 rounded-md text-xl font-mono font-light">
+      <div>
+        <p>Here goest the text to be typed</p>
+      </div>
+
+      <div>
+        <input type="text" />
+      </div>
+    </div>
+  );
 };
 
 export default TypingArea;
