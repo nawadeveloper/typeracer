b47a981fe4de533a7cdcca413cca78a8d41bfadd
nawadeveloper <nawarajusa@gmail.com>
2024-12-13 20:13:26 -0600

    added mark text logic

--- client/src/components/TextToBeTyped.tsx
+++ client/src/components/TextToBeTyped.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const TextToBeType = ({ text }: { text: string }) => {
+  const [mark, setMark] = useState({ correctlyTyped: 10, totalCharTyped: 14 });
   return (
     <p>
-      {text.split("").map((char, i) => (
-        <span key={char + i}>{char}</span>
-      ))}
+      {text.split("").map((char, i) => {
+        const textColor = i < mark.correctlyTyped ? "text-green-500" : "";
+        const bgColor =
+          i >= mark.correctlyTyped && i < mark.totalCharTyped
+            ? "bg-red-300"
+            : "";
+
+        return (
+          <span className={`${textColor} ${bgColor}`} key={char + i}>
+            {char}
+          </span>
+        );
+      })}
     </p>
   );
 };
