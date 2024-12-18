e9897a843d4f6ddb6bec17ffc1e5344e81f71d4a
nawadeveloper <nawarajusa@gmail.com>
2024-12-18 17:56:39 -0600

    car animation complete

--- client/src/components/Car.tsx
+++ client/src/components/Car.tsx
@@ -1,6 +1,14 @@
+import { useTypingStore } from "../stores/useTypingStore";
+
 const Car = () => {
+  const completePercentage = useTypingStore(
+    (state) => state.completePercentage
+  );
   return (
-    <div className="flex gap-1 pb-2">
+    <div
+      style={{ transform: `translateX(${completePercentage * 5.2 - 520}px)` }}
+      className="flex gap-1 pb-2"
+    >
       <p>(you)</p>
       <img width={60} src="car.png" />
     </div>
